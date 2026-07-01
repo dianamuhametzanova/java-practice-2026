5935cef5c8e9094fadf941dfb602c964350f6981
diana <dianamuhametzanova1790@gmail.com>
2026-07-01 21:46:49 +0300

    task 1 - findById

--- src/ru/itis/shop/user/infrastructure/persistence/UserFileRepository.java
+++ src/ru/itis/shop/user/infrastructure/persistence/UserFileRepository.java
@@ -3,9 +3,7 @@ package ru.itis.shop.user.infrastructure.persistence;
 import ru.itis.shop.user.domain.User;
 import ru.itis.shop.user.repository.UserRepository;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.UUID;
 
 public class UserFileRepository implements UserRepository {
@@ -33,6 +31,22 @@ public class UserFileRepository implements UserRepository {
 
     @Override
     public User findById(String id) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+
+            String line;
+            while ((line = reader.readLine()) != null) {
+
+                String[] parts = line.split("\\|");
+
+                if (parts[0].equals(id)) {
+                    return new User(parts[0], parts[1], parts[2], parts[3]);
+                }
+            }
+
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
+
         return null;
     }
 }
