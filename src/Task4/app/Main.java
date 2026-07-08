63930948a059a802794e14d8fba8a113d974d6ef
diana <dianamuhametzanova1790@gmail.com>
2026-07-08 17:57:23 +0300

    task 4 - findAll

--- src/Task4/app/Main.java
+++ src/Task4/app/Main.java
@@ -1,14 +1,15 @@
 package Task4.app;
 
-import Task2.ru.itis.shop.user.api.UserConsoleOperations;
-import Task2.ru.itis.shop.user.application.UserService;
-import Task2.ru.itis.shop.user.infrastructure.persistence.UserFileRepository;
-import Task2.ru.itis.shop.user.infrastructure.persistence.UserMapper;
+import Task4.user.api.UserConsoleOperations;
+import Task4.user.application.UserService;
+import Task4.user.infrastructure.persistence.UserFileRepository;
+import Task4.user.infrastructure.persistence.UserMapper;
+import Task4.user.infrastructure.persistence.UserRepositoryJdbcImpl;
 
 public class Main {
     public static void main(String[] args) {
-        UserFileRepository userFileRepository = new UserFileRepository("users.txt", new UserMapper());
-        UserService userService = new UserService(userFileRepository);
+        UserRepositoryJdbcImpl userRepositoryJdbc = new UserRepositoryJdbcImpl("jdbc:postgresql://localhost:5432/java_2026", "diana", "56971729");
+        UserService userService = new UserService(userRepositoryJdbc);
 
         UserConsoleOperations operations = new UserConsoleOperations(userService);
 
